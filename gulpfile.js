a610d0b46e7d61db809ce4f44c291b8c660204cb
Artur Trifonov <artur@uniteddesign.ru>
2019-05-14 02:22:01 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        // .pipe(cssmin())
+        .pipe(gulp.dest('dist/static/css'))
+        .pipe(livereload());
+});
+gulp.task('html', function () {
+    return gulp.src('**/*.html').pipe(livereload());
+});
+
+gulp.task('default', function () {
+    livereload.listen();
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('**/*.html', gulp.series('html'));
+});
